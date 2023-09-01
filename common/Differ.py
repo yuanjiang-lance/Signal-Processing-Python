63400a2ac867dd0797ee8f1567b990b06d799671
yuanjiang-lance <lance682@qq.com>
2023-09-01 18:29:13 -0500

    update: delete additional codes

--- common/Differ.py
+++ common/Differ.py
@@ -11,7 +11,6 @@ def Differ(y, dx):
     dy: derivative of y, 1D Numpy array
     """
 
-    L = len(y)
     dy = (y[2:] - y[:-2]) / (2*dx)
     dy = np.insert(dy, 0, (y[1]-y[0])/dx)
     dy = np.insert(dy, len(dy), (y[-1]-y[-2])/dx)
