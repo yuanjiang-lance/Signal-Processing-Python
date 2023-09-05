f523a15de3e83af13af763a60ba628dade816e45
yuanjiang-lance <lance682@qq.com>
2023-09-05 17:01:27 -0500

    update: DFRE_F

--- common/ridges.py
+++ common/ridges.py
@@ -14,7 +14,7 @@ from scipy.sparse import diags, eye, linalg
 def DFRE(Spec, delta):
     """
     Dual Fast Ridge Estimation algorithm for time-frequency ridge detection
-    Only one ridge curve could be extracted under one excecution
+    Only one ridge curve could be extracted under one execution
     Suitable for time-related signals, e.g. harmonics
     For frequency-related (dispersive) signals, it is recommended to use DFRE_F
 
@@ -44,9 +44,49 @@ def DFRE(Spec, delta):
     f1 = fmax
     for j in range(max(-1, tmax-1), -1, -1):
         low = max(0, f1-delta)
-        up = min(M, f0+delta+1)
+        up = min(M, f1+delta+1)
         f1 = np.argmax(Spec[low:up, j]) + low
         index[j] = f1
+
+    return index
+
+
+def DFRE_F(Spec, delta):
+    """
+    Dual Fast Ridge Estimation algorithm for time-frequency ridge detection
+    Only one ridge curve could be extracted under one execution
+    Suitable for frequency-related (dispersive) signals, e.g. Lamb waves
+    For time-related signals, it is recommended to use DFRE
+
+    ----------- Parameters -----------
+    Spec: Time-frequency Spectrum, 2D numpy array
+    delta: searching scope
+
+    ------------ Returns -------------
+    index: index of frequency bins corresponding to ridge curve
+    """
+
+    Spec = np.abs(Spec)
+    M, N = Spec.shape
+    index = np.zeros(M, dtype=int)
+    fmax, tmax = np.unravel_index(np.argmax(Spec), Spec.shape)
+    index[fmax] = tmax
+
+    # Extracting ridge points in higher frequency region
+    t0 = tmax
+    for j in range(min(fmax+1, M), M):
+        low = max(0, t0-delta)
+        up = min(M, t0+delta+1)
+        t0 = np.argmax(Spec[low:up, j]) + low
+        index[j] = t0
+
+    # Extracting ridge points in lower frequency region
+    t1 = tmax
+    for j in range(max(-1, fmax-1), -1, -1):
+        low = max(0, t1-delta)
+        up = min(M, t1+delta+1)
+        t1 = np.argmax(Spec[low:up, j]) + low
+        index[j] = t1
 
     return index
 
